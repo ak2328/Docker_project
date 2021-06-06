5db06080f742d9a9d54454bee351e9f9567f8086
root <root@ip-192-168-45-14.ap-south-1.compute.internal>
2021-06-06 22:41:39 +0000

    commiting new files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Back to Good Work!
+          We are testing the Github workflow!
         </p>
         <a
           className="App-link"
